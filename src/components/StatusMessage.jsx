69c22439e7201d5a39b7a9d8c39a53e673d5a41b
DeV <debanjan.das90@gmail.com>
2023-07-10 23:56:43 +0530

    color status message X and O

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -2,7 +2,12 @@ export default function StatusMessage({ isXNext, winner }) {
   return (
     <section className="status-message">
       {winner ? (
-        `Winner is ${winner}`
+        <>
+          Winner is{" "}
+          <span className={winner === "X" ? "text-green" : "text-orange"}>
+            {winner}
+          </span>
+        </>
       ) : (
         <>
           Next player is{" "}
